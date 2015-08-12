9bc549002e086c62cef12ada12d6b5befcce5fee
moriony <demateus@gmail.com>
2015-08-12 13:09:38 +0300

    additional php 5.3 compatibility fixes

--- tests/Converter/WeightTest.php
+++ tests/Converter/WeightTest.php
@@ -34,39 +34,39 @@ class WeightTest extends \PHPUnit_Framework_TestCase
 
     public function provideInvalidUnits()
     {
-        return [
-            ['invalid unit', Weight::KG],
-            [Weight::KG, 'invalid unit'],
-            ['invalid unit', 'invalid unit'],
-        ];
+        return array(
+            array('invalid unit', Weight::KG),
+            array(Weight::KG, 'invalid unit'),
+            array('invalid unit', 'invalid unit'),
+        );
     }
 
     public function provideConvertData()
     {
-        return [
+        return array(
             // KG
-            [10, Weight::KG, Weight::KG, 10],
-            [10, Weight::KG, Weight::G, 10000],
-            [10, Weight::KG, Weight::LB, 22.04623],
-            [10, Weight::KG, Weight::OZ, 352.73369],
+            array(10, Weight::KG, Weight::KG, 10),
+            array(10, Weight::KG, Weight::G, 10000),
+            array(10, Weight::KG, Weight::LB, 22.04623),
+            array(10, Weight::KG, Weight::OZ, 352.73369),
 
             // G
-            [10, Weight::G, Weight::KG, 0.01],
-            [10, Weight::G, Weight::G, 10],
-            [10, Weight::G, Weight::LB, 0.02204623],
-            [10, Weight::G, Weight::OZ, 0.35273369],
+            array(10, Weight::G, Weight::KG, 0.01),
+            array(10, Weight::G, Weight::G, 10),
+            array(10, Weight::G, Weight::LB, 0.02204623),
+            array(10, Weight::G, Weight::OZ, 0.35273369),
 
             // LB
-            [10, Weight::LB, Weight::KG, 4.535922921969],
-            [10, Weight::LB, Weight::G, 4535.922921969],
-            [10, Weight::LB, Weight::LB, 10],
-            [10, Weight::LB, Weight::OZ, 159.99728298217],
+            array(10, Weight::LB, Weight::KG, 4.535922921969),
+            array(10, Weight::LB, Weight::G, 4535.922921969),
+            array(10, Weight::LB, Weight::LB, 10),
+            array(10, Weight::LB, Weight::OZ, 159.99728298217),
 
             // OZ
-            [10, Weight::OZ, Weight::KG, 0.28349999683898],
-            [10, Weight::OZ, Weight::G, 283.49999683898],
-            [10, Weight::OZ, Weight::LB, 0.62501061353113],
-            [10, Weight::OZ, Weight::OZ, 10],
-        ];
+            array(10, Weight::OZ, Weight::KG, 0.28349999683898),
+            array(10, Weight::OZ, Weight::G, 283.49999683898),
+            array(10, Weight::OZ, Weight::LB, 0.62501061353113),
+            array(10, Weight::OZ, Weight::OZ, 10),
+        );
     }
 }
